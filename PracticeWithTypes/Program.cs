4883861da05986c62e19e4a40bf5c514c348234b
CENTURION 1 <kdtkachenko@gmail.com>
2024-09-20 01:12:12 +0300

    Добавляем ветку для практического задания по Типам значений и ссылочным типам

---
+++ PracticeWithTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using BankSystemApplication;
+using System.Security.Cryptography.X509Certificates;
+
+static void Main(string[] args)
+{
+    Person person = new() { Name = "Stas", Age = 22, Id = 10 };
+    Employee employee = new() { Contract = "" };
+    
+    
+}
