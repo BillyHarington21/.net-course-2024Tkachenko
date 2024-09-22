132e01c4e10ae36e62c798b04f4565b1f8a946fb
CENTURION 1 <kdtkachenko@gmail.com>
2024-09-22 23:23:13 +0300

    Перезаливаю исправленный код в новую ветку. Модели вынесены в самостоятельные файлы, Лишние методы удалены, все исправлено для четкого соответсвия с требованиями заданий.

---
+++ BankSystemApplication/Modelss/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemDomen.Modelss
+{
+    public struct Currency
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+    }
+}
